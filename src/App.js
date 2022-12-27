c474ce0a9ed8e42d0fe8a4e35de570ac251897cc
danie <daniel.back9@outlook.com>
2022-12-27 09:24:55 +0100

    This is just a Mock project using React.js trying out diffrerent layouts and functionalities. The Website is meant to simulate a Coding learning platform.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactasdas
         </a>
       </header>
     </div>
